018b22b8a19f281cab7c5d3c2aa94c29462bcfc1
Eduardo Martini <123970080+duudumartini@users.noreply.github.com>
2023-10-24 21:28:07 -0300

    Update

--- Conversor-De-Cambio/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Conversor-De-Cambio/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -158,7 +158,7 @@ namespace Conversor_De_Cambio {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Conversor-De-Cambio;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Conversor-De-Cambio;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
